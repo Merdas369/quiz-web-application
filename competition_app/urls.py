52bd08d571a68f70da66c787b135f49e4be59f21
Merdas369 <3epahvand@gmail.com>
2026-02-21 10:59:52 +0330

    making the urls and placeholder views

--- competition_app/urls.py
+++ competition_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import main_page
+from .views import home, quiz_start, quiz_result, quiz_question, leaderboard
 
 urlpatterns = [
-    path("", main_page)
+    path("", home, name= "home"),
+    path("quiz_start/<int:category_id>/", quiz_start, name= "quiz_start"),
+    path("quiz/<int:quiz_id>/question/<int:num>/", quiz_question, name= "quiz_question"),
+    path("quiz/<int:quiz_id>/result/", quiz_result, name= "quiz_result"),
+    path("leaderboard/", leaderboard, name= "leaderboard"),
 ]
--- competition_app/views.py
+++ competition_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 
@@ -7,5 +8,21 @@ api = "https://opentdb.com/api.php?amount=5&category=9&type=multiple"
 # Create your views here.
 
 
-def main_page(req):
-    return render(req, "competition_app/index.html")
+def home(req):
+    return HttpResponse("<h1>home page</h1>")
+
+
+def quiz_start(req, category_id):
+    return HttpResponse("<h1>quiz_page</h1>")
+
+
+def quiz_question(req, quiz_id, num):
+    return HttpResponse("<h1>quiz_question</h1>")
+
+
+def quiz_result(req, quiz_id):
+    return HttpResponse("<h1>quiz_result</h1>")
+
+
+def leaderboard(req):
+    return HttpResponse("<h1>leaderboard</h1>")
